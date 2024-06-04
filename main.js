9a13b6b7a543afe2f7954c90e2ec8405f52871a4
rs-pro0 <rostyslav.yabchenko@gmail.com>
2024-06-04 19:16:37 +0300

    boom

--- main.js
+++
@@ -1,6 +0,0 @@
-const nav = document.querySelector(".navigation")
-const burgerBtn = document.querySelector(".burger-button")
-
-burgerBtn.addEventListener("click", () => {
-    nav.classList.toggle("active")
-})
